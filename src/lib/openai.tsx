3b4a27e902f5a37d535f9f82f279af599a0f1934
yang <zy892065502@gmail.com>
2023-10-09 18:39:32 +0800

    use serverless

--- src/lib/openai.tsx
+++ src/lib/openai.tsx
@@ -1,7 +1,7 @@
 // lib/openAiService.tsx
 
-const openAiEndpoint = `https://api.openai.com/v1/chat/completions`;
-// const openAiEndpoint = `https://api.autumnriver.chat/api/v1/post`;
+// const openAiEndpoint = `https://api.openai.com/v1/chat/completions`;
+const openAiEndpoint = `https://api.autumnriver.chat/api/v1/post`;
 
 export interface ConversationMessage {
   role: string;
